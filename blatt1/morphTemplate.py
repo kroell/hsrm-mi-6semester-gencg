6436f0f98c992fdaa9ce56ab58aa252aa8af3c22
Soeren Kroell <kroell@idseven.de>
2013-04-10 11:41:21 +0200

    aufgeräumt

--- blatt1/morphTemplate.py
+++ blatt1/morphTemplate.py
@@ -1,5 +1,9 @@
 '''
 Created on 09.04.2013
+
+Generative Computergrafik, Uebungsblatt 1, Aufgabe 5
+Hochschule RheinMain, Medieninformatik
+
 @author: soerenkroell
 '''
 
@@ -13,26 +17,24 @@ import sys
 import copy
 
 
-
 WIDTH  = 400 # width of canvas
 HEIGHT = 400 # height of canvas
 
 HPSIZE = 2 # half of point size (must be integer)
 CCOLOR = "#0000FF" # blue
 
-elementList = [] # list of elements (used by Canvas.delete(...))
+ELEMENT_LIST = [] # list of elements (used by Canvas.delete(...))
 
 #polygon = [[50,50],[350,50],[350,350],[50,350],[50,50]]
+#time = 0
 
-time = 0
-dt = 0.01
+DT = 0.01
 
 def drawObjects():
-    """ draw polygon and points """
-    # TODO: inpterpolate between polygons and render
+    "draw polygon and points"
     for (p,q) in zip(polygon,polygon[1:]):
-        elementList.append(can.create_line(p[0], p[1], q[0], q[1], fill=CCOLOR))
-        elementList.append(can.create_oval(p[0]-HPSIZE, p[1]-HPSIZE, p[0]+HPSIZE, p[1]+HPSIZE, fill=CCOLOR, outline=CCOLOR))
+        ELEMENT_LIST.append(can.create_line(p[0], p[1], q[0], q[1], fill=CCOLOR))
+        ELEMENT_LIST.append(can.create_oval(p[0]-HPSIZE, p[1]-HPSIZE, p[0]+HPSIZE, p[1]+HPSIZE, fill=CCOLOR, outline=CCOLOR))
             
             
 def quitProgram(root=None):
@@ -42,28 +44,20 @@ def quitProgram(root=None):
     root._root().quit()
     root._root().destroy()
 
-
 def draw():
     "Elemente zeichnen"
-    can.delete(*elementList)
-    del elementList[:]
+    can.delete(*ELEMENT_LIST)
+    del ELEMENT_LIST[:]
     drawObjects()
     can.update()
 
-
 def forward():
     "Vorwaerts morphen"
-    global time
     time = 0
-    print "P-f: ", polygon
-    print "A-f: ", polygonA
-    print "Adc-b: ", polygonAdc
-    print "Z-f: ", polygonZ
-    print "Zdc-f: ", polygonZdc
 
     while(time < 1):
-        time += dt
-        # TODO: interpolate 
+        time += DT
+        # interpolate 
         for i in range(len(polygon)): 
             polygon[i][0] = (1 - time) * polygonAdc[i][0] + time * polygonZdc[i][0]
             polygon[i][1] = (1 - time) * polygonAdc[i][1] + time * polygonZdc[i][1]
@@ -72,27 +66,19 @@ def forward():
 
 def backward():
     "Rueckwaerts morphen"
-    global time
     time = 0
-    print time
-    print "P-b: ", polygon
-    print "A-b: ", polygonA
-    print "Adc-b: ", polygonAdc
-    print "Z-b: ", polygonZ
-    print "Zdc-b: ", polygonZ
   
-    #polygon = polygonA
     while(time < 1):
-        time += dt
+        time += DT
+        # interpolate 
         for i in range(len(polygon)): 
             polygon[i][0] = (1 - time) * polygonZdc[i][0] + time * polygonAdc[i][0]
             polygon[i][1] = (1 - time) * polygonZdc[i][1] + time * polygonAdc[i][1]
     
         draw()
 
-
 def readFile(fileName):
-    "Datei einlesen und Inhalt als Liste [[float,float]] zurueckgeben"
+    "Datei einlesen und Inhalt als Liste [[x,y]] zurueckgeben"
     # Ausgabe Dateinamen
     print "Polygondatei: ", fileName
     
@@ -102,9 +88,8 @@ def readFile(fileName):
     for i in f: 
         n = i.split()
         #x,y Koordinaten als float in Liste schreiben
-        lis.append(map(float, n[::]))
+        lis.append(map(float, n))
     return lis
-
 
 def localToGlobal(lis):
     "Von lokalem in globales Koordinatensystem umwandeln"
@@ -115,7 +100,6 @@ def localToGlobal(lis):
         #die neuen x und y Werte setzen
         i[0] = x
         i[1] = y
-
 
 
 if __name__ == "__main__":
@@ -131,7 +115,6 @@ if __name__ == "__main__":
     # - transform from local into global coordinate system 
     localToGlobal(polygonA)
     localToGlobal(polygonZ)
-
     
     # - make both polygons contain same number of points
     if len(polygonA) > len(polygonZ):
@@ -140,15 +123,12 @@ if __name__ == "__main__":
         polygonA.append(polygonA[0])
     
     # polygonA als Anfang setzen
-    # kopie von A
     polygon = polygonA[:]
     
+    # tiefe Kopie von A und Z erstellen
     polygonAdc = copy.deepcopy(polygonA)
     polygonZdc = copy.deepcopy(polygonZ)
 
-    if polygon is polygonA:
-        print "Polygon und Polygon A sind identisch"
-    
     # GUI
     mw = Tk()
     mw._root().wm_title("Morphing")
