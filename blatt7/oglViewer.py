f197ac795dd9966712fdfdde394f682d89d5fdb6
soerenkroell <kroell@idseven.de>
2013-05-29 17:37:58 +0200

    abgabe weiter bearbeitet

--- blatt7/oglViewer.py
+++ blatt7/oglViewer.py
@@ -1,35 +1,44 @@
 '''
 Created on May 23, 2013
-Finished on May 30, 2013
+Finished on May 31, 2013
 
 Generative Computergrafik, Uebungsblatt 7, Aufgabe 1
 Bewertete Abgabe
 Hochschule RheinMain, Medieninformatik
 
+@author: Soeren Kroell
+
+
 Start des Programms mit:
-- python object.obj [wire|solid]
+- python objectFile.obj [wire|solid]
 
 Standardwerte:
 - Objektdarstellung als wire 
 - Intrinsische Kamera auf orthographische Projektion
 
-Folgende Features sind enthalten:
-- Aendern der Hintergrundfarbe mit 'b'
-- Aendern der Vordergrundfabre mit 'f'
-- Objekt rotieren mit linker Muastaste
-- Objekt zoom mit mittlerer Maustaste
-- Objekt verschieben mit rechter Maustaste
-- Umstellen der intrinsischen Kamera auf orthographische Projektion mit 'o'
-- Umstellen der intrinsischen Kamera auf perspektivische Projektion mit 'p'
+Folgende Tastaturbelegungen sind enthalten:
+- b:    Aendern der Hintergrundfarbe
+- f:    Aendern der Vordergrundfabre
+- o:    Umstellen der intrinsischen Kamera auf orthographische Projektion
+- p:    Umstellen der intrinsischen Kamera auf perspektivische Projektion
+- x:    im Uhrzeigersinn um die x-Achse zu drehen
+- X:    gegen den Uhrzeigersinn um die x-Achse zu drehen
+- y:    im Uhrzeigersinn um die y-Achse zu drehen
+- Y:    gegen den Uhrzeigersinn um die y-Achse zu drehen
+- z:    im Uhrzeigersinn um die z-Achse zu drehen
+- Z:    gegen den Uhrzeigersinn um die z-Achse zu drehen
+- w:    Objekt als Polygonnetz darstellen
+- s:    Objekt ausgefuellt anzeigen
+- l:    Licht einschalten (Objekt kann dann nicht mehr mit f eingefaerbt werden)
+- k:    Licht ausschalten
+- ESC:  Frame schliessen und Programm beenden 
 
-Zusatzfeature:
-- Objekt solid und beleuchtet darstellen, dazu wie folgt starten
-  python object.obj solid
-  
-  Die Objektfarbe kann dann jedoch nicht mehr geandert werden!
+Folgende Mausbelegungen sind enthalten:
+- linke Muastaste:       Objekt rotieren
+- Mittlere Maustaste:    Objekt zoom
+- Rechte Maustaste:      Objekt verschieben
 
-
-@author: Soeren Kroell
+ 
 '''
 
 from OpenGL.GL import *
@@ -40,6 +49,8 @@ from numpy import array
 
 import numpy, math
 
+
+# Define globals
 my_vbo = None
 
 doZoom = False
@@ -49,19 +60,25 @@ solidMode = False
 wireMode = True
 orthoMode = True
 perspectiveMode = False
+light = False
 
-actOri = 1
-axis = [1,1,1]
+mouseLastX = None
+mouseLastY = None
+
+#actOri = 1
+#axis = [0.0, 1.0, 0.0]
 angle = 10
-newPosition = [0,0,0]
 
-zPosCamera =0.0
+newXPos = 0.0
+newYPos = 0.0
+
+zoomFactor = 0
 
 frontColorIndex,backColorIndex = 2,1
 rotateX, rotateY, rotateZ = 0, 0, 0
 WIDTH, HEIGHT = 500, 500
 
-aspect = WIDTH/HEIGHT
+aspect = float(WIDTH/HEIGHT)
 fov = 60
 near = 0.1
 far = 30.0
@@ -84,17 +101,8 @@ def initGL(width, height):
     '''
     OpenGL initialize
     '''
-    global solidMode
-    
     #Set background color - black
     glClearColor(colorList[0][0],colorList[0][1],colorList[0][2],colorList[0][3])
-    
-    if solidMode:
-        glEnable(GL_LIGHTING)
-        glEnable(GL_LIGHT0)
-        glEnable(GL_DEPTH_TEST)
-        glEnable(GL_NORMALIZE)
-
     #switch to projection matrix
     glMatrixMode(GL_PROJECTION)
     #set to 1
@@ -105,99 +113,83 @@ def initGL(width, height):
     glMatrixMode(GL_MODELVIEW)
 
 
-def projectOnSphere(x,y,r):
-    '''
-    '''
-    x,y = x - WIDTH / 2.0, HEIGHT/ 2.0 -y
-    a = min(r*r, x**2 + y**2)
-    z = math.sqrt(r*r - a)
-    l = math.sqrt(x**2 + y**2 + z**2)
-    return x/l, y/l, z/l
-
-
 def mouse(button, state, x, y):
     '''
     handle mouse events
     '''
-    global startP, actOri, angle, doRotation, axis, doZoom, doRotation, doTranslation
-    r = min(WIDTH, HEIGHT) / 2.0
+    global doRotation, doZoom, doRotation, doTranslation, mouseLastX, mouseLastY
+    
+    mouseLastX, mouseLastY = None, None
+    
     # rotate object
     if button == GLUT_LEFT_BUTTON:
         if state == GLUT_DOWN:
             doRotation = True
-            startP = projectOnSphere(x,y,r)
         if state == GLUT_UP:
             doRotation = False
-            actOri = actOri * rotate(angle,axis)
-            angle = 0
             
     # translate object
-    if button == GLUT_RIGHT_BUTTON:
+    if button == GLUT_MIDDLE_BUTTON:
         if state == GLUT_DOWN:
             doTranslation = True
         if state == GLUT_UP:
             doTranslation = False
         
     # zoom object
-    if button == GLUT_MIDDLE_BUTTON:
+    if button == GLUT_RIGHT_BUTTON:
         if state == GLUT_DOWN:
             doZoom = True
         if state == GLUT_UP:
             doZoom = False
 
+
 def mouseMotion(x,y):
     ''' 
     handle mouse motion
     '''
-    global angle, axis, scaleFactor, doZoom, doRotation, doTranslation, center, newPosition
+    global angle, doZoom, doRotation, doTranslation, center, mouseLastX, mouseLastY, rotateX, rotateY, frameWidth, frameHeight, newXPos, newYPos, zoomFactor
     
-    # rotate by mouse
-    if doRotation:
-        r = min(WIDTH, HEIGHT) / 2.0
-        moveP = projectOnSphere(x, y, r)
-        #math domain error
-        angle = math.acos(numpy.dot(startP,moveP))
-        axis = numpy.cross(startP, moveP)
-        glutPostRedisplay()
+    xDiff = 0
+    yDiff = 0
+    
+    # calc difference between act and last x,y mouse coordinates
+    if mouseLastX != None:
+        xDiff = x - mouseLastX
+    if mouseLastY != None:
+        yDiff = y - mouseLastY
+    
+    # rotate
+    if doRotation:        
+        if xDiff != 0:
+            rotateY += xDiff
+        if yDiff != 0:
+            rotateX += yDiff
     
     # zoom  
-    if doZoom:
-        r = min(WIDTH, HEIGHT) / 2.0
-        moveP = projectOnSphere(x, y, r)
-        x,y,z = [i for i in moveP]
-        # differenz anfangspunkt zu endpunkt
-        zoomFactor = y/100
-        print "zoomFactor: ", zoomFactor
-        if zoomFactor >= 0.0001 and zoomFactor <= 0.01:
-            scaleFactor= zoomFactor
-            glutPostRedisplay()
+    if doZoom:  
+        zScale = float(frameHeight) / angle
+        if yDiff != 0:
+            zoomFactor += yDiff / zScale
+            # limit zoomFactor
+            if zoomFactor <= -1.90:
+                zoomFactor = -1.90
+            if zoomFactor >= 2.00:
+                zoomFactor = 1.99
     
     # translatation
     if doTranslation:
-        r = min(WIDTH, HEIGHT) / 2.0
-        moveP = projectOnSphere(x, y, r)
-        x,y,z = [i for i in moveP]
-        newPosition = [x,y,z]
-        glutPostRedisplay()
+        scale = float(frameWidth)/2.0
+        if xDiff != 0:
+            newXPos += xDiff / scale
+        if yDiff != 0:
+            newYPos += -yDiff / scale
     
-        
-def rotate(angle,axis):
-    '''
-    rotate object
-    '''
-    c, mc = math.cos(angle), 1-math.cos(angle)
-    s = math.sin(angle)
-    l = math.sqrt(numpy.dot(numpy.array(axis), numpy.array(axis)))
-    if l != 0:
-        x,y,z = numpy.array(axis)/l
-        r = numpy.matrix([
-                         [x*x*mc+c, x*y*mc-z*s, x*z*mc+y*s, 0],
-                         [x*y*mc+z*s, y*y*mc+c, y*z*mc-x*s, 0],
-                         [x*z*mc-y*s, y*z*mc+x*s, z*z*mc+c, 0],
-                         [0,0,0,1]
-                         ])
-    return r.transpose()
-        
+    # Remember last x,y mouse coordinates
+    mouseLastX = x
+    mouseLastY = y
+    
+    glutPostRedisplay()
+           
 
 def initGeometryFromObjFile():
     '''
@@ -217,7 +209,6 @@ def initGeometryFromObjFile():
     objectNormals = []
     objectFaces =[] 
     data = []
-    objectFaces2 = []
     
     for lines in file(objFile):
         # wenn nicht leer
@@ -229,18 +220,18 @@ def initGeometryFromObjFile():
                 objectNormals.append(map(float,lines.split()[1:]))
             if check == 'f':
                 first = lines.split()[1:]
-                if not objectNormals:
-                    objectFaces2.append(map(float,first))
                 for face in first:
                     objectFaces.append(map(float,face.split('//')))
-    
-    print "objFaces: ",objectFaces[:10]
-    print "objFaces2: ",objectFaces2[:10]
-    
+
     for face in objectFaces:
+        # if no vt is available
         if len(face) == 2:
             face.insert(1, 0.0)
-
+        # if no vt and no vn is available
+        if len(face) == 1:
+            face.insert(1, 0.0)
+            face.insert(2, 0.0)
+    
     # Create BoundingBox
     boundingBox = [map(min, zip(*objectVertices)), map(max, zip(*objectVertices))]
     # Calc center of bounding box
@@ -248,47 +239,83 @@ def initGeometryFromObjFile():
     # Calc scale factor
     scaleFactor = 2.0/max([(x[1]-x[0]) for x in zip(*boundingBox)])
     
-    if objectNormals:
-        for vertex in objectFaces:
-            vn = int(vertex[0])-1
-            nn = int(vertex[2])-1
+    # get the right data for the vbo
+    for vertex in objectFaces:
+        vn = int(vertex[0])-1
+        nn = int(vertex[2])-1
+        if objectNormals:
             data.append(objectVertices[vn] + objectNormals[nn])
-    else:
-        for vertex in objectFaces2:
-            point = []
-            for x in vertex:
-                vn = int(x)-1
-                point.append(objectVertices[vn])
-            data.append(point)
-            
-    print "verticies: ", objectVertices[:10]
-    print "data: ",data[:10]
+        else:
+            temp = [0.0,0.0,0.0]
+            data.append(objectVertices[vn] + temp)
     
     my_vbo = vbo.VBO(array(data,'f'))
-        
+
+
+def loadOBJ(filename):
+    numVerts = 0
+    verts = []
+    norms = []
+    vertsOut = []
+    normsOut = []
+    for line in open(filename, "r"):
+        vals = line.split()
+        if vals[0] == "v":
+            v = map(float, vals[1:4])
+            verts.append(v)
+        if vals[0] == "vn":
+            n = map(float, vals[1:4])
+            norms.append(n)
+        if vals[0] == "f":
+            for f in vals[1:]:
+                w = f.split("/")
+                # OBJ Files are 1-indexed so we must subtract 1 below
+                vertsOut.append(list(verts[int(w[0])-1]))
+                normsOut.append(list(norms[int(w[2])-1]))
+                numVerts += 1
+    return vertsOut, normsOut
+
 
 def display():
     '''
-    Render objects
+    Render all objects
     '''
-    global scaleFactor, center, my_vbo, actOri, angle, axis, data, newPosition, wireMode, orthoMode, perspectiveMode
+    global scaleFactor, center, my_vbo, actOri, angle, axis, data, wireMode, orthoMode, perspectiveMode, solidMode, light, newXPos, newYPos, zoomFactor
 
     # Clear framebuffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
     # view as ortho Projection
     if orthoMode:
+        if light:
+            glEnable(GL_LIGHTING)
+            glEnable(GL_LIGHT0)
+            glEnable(GL_COLOR_MATERIAL)
+            glEnable(GL_DEPTH_TEST)
+            glEnable(GL_NORMALIZE)
+        else:
+            glDisable(GL_LIGHTING)
+            
         #switch to projection matrix
         glMatrixMode(GL_PROJECTION)
         #set to 1
         glLoadIdentity()
         # Camera, multiply with new p-matrix
+        
+        # left, right, bottom, top,
         gluOrtho2D(-1.5, 1.5, -1.5, 1.5)
         #switch to modelview matrix
         glMatrixMode(GL_MODELVIEW)
     
     # view as perspective Projection
     if perspectiveMode:
+        if light:
+            glEnable(GL_LIGHTING)
+            glEnable(GL_LIGHT0)
+            glEnable(GL_DEPTH_TEST)
+            glEnable(GL_NORMALIZE)
+        else:
+            glDisable(GL_LIGHTING)
         #switch to projection matrix
         glMatrixMode(GL_PROJECTION)
         #set to 1
@@ -296,7 +323,7 @@ def display():
         #change perspective mode
         gluPerspective(fov, aspect, near, far)
         #set Camera
-        gluLookAt (0,0,2,0,0,0,0,1,0)
+        gluLookAt (0,0,3,0,0,0,0,1,0)
         #switch to modelview matrix
         glMatrixMode(GL_MODELVIEW)
     
@@ -312,28 +339,29 @@ def display():
     glLoadIdentity()
 
     # Translate
-    glTranslate(newPosition[0],newPosition[1],0)
+    glTranslate(newXPos,newYPos,0.0)
     
-    # Rotate by key
+    # Rotate
     glRotate(rotateX, 1, 0, 0)
     glRotate(rotateY, 0, 1, 0)
     glRotate(rotateZ, 0, 0, 1)
     
-    # Rotate by mouse
-    glMultMatrixf(actOri*rotate(angle,axis))
-    
-    # Scale
-    glScale(scaleFactor, scaleFactor, scaleFactor)
-    
+    # Scale + Zoom
+    glScale(scaleFactor+zoomFactor, scaleFactor+zoomFactor, scaleFactor+zoomFactor)
     # move to center
     glTranslate(-center[0], -center[1], -center[2])
-    
+            
     # show object as wires
     if wireMode:
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        # Draw VBO as Triangles
+        glDrawArrays(GL_TRIANGLES, 0, len(data))
     
-    # Draw VBO as Triangles
-    glDrawArrays(GL_TRIANGLES, 0, len(data))
+    # show object as solid
+    if solidMode:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        # Draw VBO as Triangles
+        glDrawArrays(GL_TRIANGLES, 0, len(data))
 
     my_vbo.unbind()
     glDisableClientState(GL_VERTEX_ARRAY)
@@ -345,9 +373,9 @@ def display():
 
 def keyPressed(key, x, y):
     '''
-    Hintergrundfarbe mit b und Objektfarbe mit f wechseln
+    Handle keypress events
     '''
-    global colorList, frontColorIndex, backColorIndex, angle, rotateX, rotateY, rotateZ, perspectiveMode, orthoMode
+    global colorList, frontColorIndex, backColorIndex, rotateX, rotateY, rotateZ, perspectiveMode, orthoMode, solidMode, gridMode, light
 
     # If escape is pressed, kill everything.
     if key == '\x1b':
@@ -370,7 +398,7 @@ def keyPressed(key, x, y):
         else:
             frontColorIndex = 1
             glColor(colorList[0][0],colorList[0][1],colorList[0][2])
-
+    
     # Rotate with keys x,X,y,Y,z,Z
     if key == 'x': 
         rotateX = rotateX + angle
@@ -384,6 +412,24 @@ def keyPressed(key, x, y):
         rotateZ = rotateZ + angle
     if key == 'Z':
         rotateZ = rotateZ - angle
+    
+    # Show Object in WireMode
+    if key == 'w':
+        wireMode = True
+        solidMode = False
+    
+    # Show Object in SolidMode
+    if key == 's':
+        solidMode = True
+        wireMode = False
+    
+    # Turn on light
+    if key == 'l':
+        light = True
+            
+    # Turn off light
+    if key == 'k':
+        light = False
     
     # Activate Orthogonal-Projection
     if key == 'o':
@@ -404,71 +450,48 @@ def resizeViewport(width, height):
     '''
     Adjust projection matrix to window size
     '''
+    global frameWidth, frameHeight
+    
     if height == 0:
         height = 1
-        
+    
+    frameWidth = width
+    frameHeight = height
+    
     glViewport(0, 0, width, height)
-    #change to projection matrix
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     
-    if width <= height:
-        glOrtho(-1.5, 1.5, -1.5*height/width, 1.5*height/width, -1.0, 1.0)
-    else:
-        glOrtho(-1.5*width/height, 1.5*width/height,-1.5, 1.5,-1.0, 1.0)
+    aspect = float(width)/height
+    viewportHeight = glViewport(0, (height - width) / 2, width, width)
+    viewportWidth = glViewport((width - height) / 2, 0, height, height)
+    
+    if orthoMode:
+        if width <= height:
+            viewportHeight
+            glOrtho(-1.5, 1.5, -1.5*height/width, 1.5*height/width, -1.0, 1.0)
+        else:
+            viewportWidth
+            glOrtho(-1.5 * aspect, 1.5 * aspect, -1.5, 1.5, -1.0, 1.0)
         
-    #change to modelview matrix
+    if perspectiveMode:
+        if width <= height:
+            viewportHeight
+            gluPerspective(45.,aspect,0.1,100.0)
+        else:
+            viewportWidth
+            gluPerspective(45.,aspect,0.1,100.0)
+            
     glMatrixMode(GL_MODELVIEW)
     
     #swap buffer
-    glutSwapBuffers()   
-
-
-def reshape(width, height):
-    """ adjust projection matrix to window size"""
-    #global windowWidth, windowHeight
-    
-    if height == 0:
-        height = 1
-        
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    
-    if orthoMode:
-        
-        if width <= height:
-            glOrtho(-1.5, 1.5, -1.5*height/width, 1.5*height/width, -1.0, 1.0)
-        else:
-            glOrtho(-1.5*width/height, 1.5*width/height,-1.5, 1.5,-1.0, 1.0)
-        
-    else:
-        if width <= height:
-            glViewport(0, (height - width) / 2, width, width)
-            gluPerspective(60.0, 1, 0.1, 100.0)
-        else:
-            glViewport((width - height) / 2, 0, height, height)
-            gluPerspective(60.0, 1.0, 0.1, 100.0)
-            
-    glMatrixMode(GL_MODELVIEW)
+    glutSwapBuffers()  
 
 
 def main():
     '''
     Fenster initialisieren, File einlesen, BoundingBox erstellen/zentrieren/skalieren
     '''
-    
-    global solidMode, wireMode
-    
-    # Wenn solid oder wire beim Start mitgegeben wurde, den entsprechenden Modus aktivieren
-    if len(sys.argv) == 3:
-        if sys.argv[2] == "solid":
-            solidMode = True
-            wireMode = False
-        if sys.argv[2] == "wire":
-            wireMode = True
-            solidMode = False
-    
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize (500,500)
@@ -476,7 +499,7 @@ def main():
     # Register display callback function
     glutDisplayFunc(display)
     # Register reshape callback function
-    glutReshapeFunc(reshape)
+    glutReshapeFunc(resizeViewport)
     # Register keyboad callback function
     glutKeyboardFunc(keyPressed)
     #register mouse function
